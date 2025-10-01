dc715cfe6c86c063a5b38c5421b2f43cc3b89609
tomiconicgit <tomiconicx@icloud.com>
2025-10-01 03:44:19 +0100

    Update camera.js

--- camera.js
+++ camera.js
@@ -9,17 +9,16 @@ export function createCamera(canvas) {
     1000 
   );
   
-  camera.position.set(2, 3, 5);
+  camera.position.set(2, 6, 8);
 
   const controls = new OrbitControls(camera, canvas);
   
   controls.enableDamping = true;
   controls.dampingFactor = 0.05;
-  controls.minDistance = 2;
+  controls.minDistance = 3;
   controls.maxDistance = 50;
   controls.maxPolarAngle = Math.PI / 2.1;
   
-  // The controls need a camera to look at a target
   controls.target.set(0, 0, 0);
 
   return { camera, controls };
