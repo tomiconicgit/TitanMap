c98ac8383d5fff0942e3e4d17c02ea9e3b68aa8d
tomiconicgit <tomiconicx@icloud.com>
2025-10-01 21:36:46 +0100

    Delete texture directory

--- texture/grass.js
+++
@@ -1,9 +0,0 @@
-// file: textures/grass.js
-import * as THREE from 'three';
-
-export function colorProc({ tx, tz, fx, fz, seed }) {
-  const base = new THREE.Color(0x2E7D32).convertSRGBToLinear();
-  // small speckle by corner
-  const speck = 1.0 + ((fx ^ fz) ? 0.06 : -0.04) + (seed - 0.5) * 0.05;
-  return base.clone().multiplyScalar(speck);
-}
