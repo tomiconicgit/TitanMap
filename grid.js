0d0e9ddb29286452dea7b4711d7db86478fb709c
tomiconicgit <tomiconicx@icloud.com>
2025-10-01 04:39:49 +0100

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,14 +1,10 @@
 import * as THREE from 'three';
 
-export function createGrid() {
-  const size = 10;
-  const divisions = 10;
+export function createGrid(size = 10, divisions = 10) { // Now accepts arguments
   const colorCenterLine = 0x888888;
   const colorGrid = 0x444444;
 
   const gridHelper = new THREE.GridHelper(size, divisions, colorCenterLine, colorGrid);
-  
   gridHelper.position.y = -0.01;
-  
   return gridHelper;
 }
